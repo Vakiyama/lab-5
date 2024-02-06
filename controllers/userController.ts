969ca06c583df2eda28c4eb4cbaacefa24c89cfd
Vakiyama <vakiyama@outlook.com>
2024-02-06 12:26:10 -0800

    fix lockfile

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,15 +1,14 @@
 import { userModel } from '../models/userModel';
 
-
 const getUserByEmailIdAndPassword = (email: string, password: string) => {
   let user = userModel.findOne(email);
   if (user) {
     if (isPasswordValid(user, password)) {
       return user;
     }
-    return "Password is invalid.";
+    return 'Password is invalid.';
   }
-  return "Email not found.";
+  return 'Email not found.';
 };
 const getUserById = (id: any) => {
   let user = userModel.findById(id);
